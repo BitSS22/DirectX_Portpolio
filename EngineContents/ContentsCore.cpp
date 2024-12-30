b4feb58bd490526ee5a73fd08e1f0f7b5ca8ba48
bitss . <donrang@naver.com>
2024-12-30 18:46:27 +0900

    update

--- EngineContents/ContentsCore.cpp
+++ EngineContents/ContentsCore.cpp
@@ -3,9 +3,8 @@
 #include <EngineCore/Level.h>
 #include <EngineCore/EngineTexture.h>
 #include <EngineCore/EngineSprite.h>
-#include "TitleGameMode.h"
+#include "PlayMode.h"
 
-// #define은 그냥 무조건 복붙
 CreateContentsCoreDefine(UContentsCore);
 
 UContentsCore::UContentsCore()
@@ -19,55 +18,14 @@ UContentsCore::~UContentsCore()
 
 void UContentsCore::EngineStart(UEngineInitData& _Data)
 {
-	// mainwindow는 아무나 건들면 안된다.
-	// 넌 컨텐츠잖아 엔진이 관리하는 윈도우라는게 존재하는지도 몰라야한다.
-
-	_Data.WindowPos = { 100, 100 };
-	_Data.WindowSize = { 1280, 720 };
-
-	{
-		UEngineDirectory Dir;
-		if (false == Dir.MoveParentToDirectory("ContentsResources"))
-		{
-			MSGASSERT("리소스 폴더를 찾지 못했습니다.");
-			return;
-		}
-		Dir.Append("Image");
-		std::vector<UEngineFile> ImageFiles = Dir.GetAllFile(true, {".PNG", ".BMP", ".JPG"});
-		for (size_t i = 0; i < ImageFiles.size(); i++)
-		{
-			std::string FilePath = ImageFiles[i].GetPathToString();
-			UEngineTexture::Load(FilePath);
-		}
-	}
-
-	UEngineSprite::CreateSpriteToMeta("Player.png", ".sdata");
-
-	{
-		UEngineDirectory Dir;
-		if (false == Dir.MoveParentToDirectory("ContentsResources"))
-		{
-			MSGASSERT("리소스 폴더를 찾지 못했습니다.");
-			return;
-		}
-		Dir.Append("Image/Tevi");
-
-		UEngineSprite::CreateSpriteToFolder(Dir.GetPathToString());
-	}
-
-
-	// 주인공 APawn 상속 받으세요.
-	UEngineCore::CreateLevel<APlayMode, APawn>("Titlelevel");
-	UEngineCore::OpenLevel("Titlelevel");
-	
+	UEngineCore::CreateLevel<APlayMode, APawn>("PlayLevel");
+	UEngineCore::OpenLevel("PlayLevel");
 }
 
 void UContentsCore::EngineTick(float _DeltaTime)
 {
-
 }
 
 void UContentsCore::EngineEnd()
 {
-
 }
